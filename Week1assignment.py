5ee392b8e1d7b36144ff5a88080c09d761903bbb
alexh29 <alexhilton29@gmail.com>
2024-07-30 19:58:41 -0400

    Add files via upload

---
+++ Week1assignment.py
@@ -0,0 +1,10 @@
+#Assigning my variables
+principal = int(input("What is your principal amount: "))
+interest = int(input("What is your interest rate (in percentage): "))
+time = int(input("How long is your time period (in years): "))
+
+#this explains the calculation
+processing = principal * interest * time/100
+
+#this displays the final answer
+print("The simple interest is:", processing)
