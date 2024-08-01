32a2e6dfa9a2d296114398ee2747874cb3d6b23b
alexh29 <alexhilton29@gmail.com>
2024-08-01 19:13:01 -0400

    Add files via upload

--- Week1assignment.py
+++ Week1assignment.py
@@ -1,10 +1,16 @@
+try:
 #Assigning my variables
-principal = int(input("What is your principal amount: "))
-interest = int(input("What is your interest rate (in percentage): "))
-time = int(input("How long is your time period (in years): "))
+    principal = int(input("What is your principal amount: "))
+    interest = int(input("What is your interest rate (in percentage): "))
+    time = int(input("How long is your time period (in years): "))
 
 #this explains the calculation
-processing = principal * interest * time/100
+    processing = principal * interest * time/100
+
 
 #this displays the final answer
-print("The simple interest is:", processing)
+    print("The simple interest is:", processing)
+
+except ValueError:
+
+    print("Please enter a numeric value")
