8db24eedc0938768cc5798a5441d9299a633b8c8
alexh29 <alexhilton29@gmail.com>
2024-08-01 19:31:18 -0400

    Add files via upload

---
+++ Week2assignment.py
@@ -0,0 +1,11 @@
+
+#input
+length = float(input("What is the length of the rectangle: "))
+width = float(input("What is the width of the rectangle: "))
+
+#processing
+processing = length * width
+
+#output
+print("The area of the rectangle is:", processing)
+
