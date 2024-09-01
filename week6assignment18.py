5c4f66fa7b6dd180c954c328cc1cdcc5259a9eec
alexh29 <alexhilton29@gmail.com>
2024-09-01 15:22:52 -0400

    Add files via upload

---
+++ week6assignment18.py
@@ -0,0 +1,7 @@
+def concat_tuples(tuple1, tuple2):
+    return tuple1 + tuple2
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+result = concat_tuples(tuple1, tuple2)
+print(result)  # Output: (1, 2, 3, 4, 5, 6)
